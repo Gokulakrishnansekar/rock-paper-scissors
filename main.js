b1fa1b9ac0b4e5efeb85ae2991b34ae61ada174b
Gokulakrishnansekar <gokulkrishna311201@gmail.com>
2022-07-19 12:38:35 +0530

    new file

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
 
 
-var rock=document.querySelectorAll("img")[0]
-rock.log(choice)
-rock.addEventListener("click",()=> console.log("clicked"))
+var choice=document.getElementsByClassName("image")
+var com=0
+function comfind(choice){
+
+
+for(var i in choice)
+{
+    console.log(i)
+   if( choice[i].addEventListener("click",function ()
+    {
+        com=i
+        if(i==0)
+        {
+            
+            console.log("you click paper")
+            return i
+        }
+        if(i==1)
+        {console.log("you click rock")}
+        if(i==2)
+        {console.log("you click scissor")}
+    }))
+    {
+        console.log("pressed")
+    }
+    
+}
+}
