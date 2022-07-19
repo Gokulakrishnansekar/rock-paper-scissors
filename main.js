a1a4912a194072f2286531a3856254ab5f388e1e
Gokulakrishnansekar <gokulkrishna311201@gmail.com>
2022-07-19 13:52:30 +0530

    final change

--- main.js
+++ main.js
@@ -1,30 +1,67 @@
 
 
+
 var choice=document.getElementsByClassName("image")
 var com=0
-function comfind(choice){
-
-
-for(var i in choice)
+let inp=document.getElementById("userinp")
+let cominp=document.getElementById("cominp")
+let op=document.querySelector("output")
+var output=""
+for(let i in choice)
 {
-    console.log(i)
-   if( choice[i].addEventListener("click",function ()
+    
+  choice[i].onclick=function ()
     {
-        com=i
-        if(i==0)
+        //console.log(choice[i])
+        let userinp=""
+        let comp=""
+       
+        userinp=findwhatval(i)
+        inp.innerHTML="You Click  "+userinp;
+        
+        var randomnum=Math.floor(Math.random()*3)
+        console.log(i)
+        console.log(randomnum)
+        comp=findwhatval(randomnum)
+        cominp.innerHTML="Computer Click  "+comp;
+        if(i==randomnum)
         {
+            console.log("draw")
+            output="draw"
+           // window.location.reload()
             
-            console.log("you click paper")
-            return i
         }
-        if(i==1)
-        {console.log("you click rock")}
-        if(i==2)
-        {console.log("you click scissor")}
-    }))
-    {
-        console.log("pressed")
+        else
+        probabilityofwin(i,randomnum)
+        op.innerHTML=output
+        window.location.reload()
+
     }
     
+    
 }
+
+function findwhatval(val)
+{
+    if(val==0)
+    return "Rock"
+    else if(val==1)
+    return "Paper"
+    else
+    return "Scissors"
+}
+function probabilityofwin(user,computer)
+{
+    console.clear()
+    if((user==0&& computer==2) || (user==1 &&computer==0)||(user==2 && computer==1))
+    {
+       
+        console.log("you won")
+        output="you won"
+    }
+    else
+    {
+        console.log("computer won")
+        output="computer won"
+    }
 }
